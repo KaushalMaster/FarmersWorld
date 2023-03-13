659e217538a8e1f5580b248db98bf8a3ef63f3ef
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2023-03-14 03:04:00 +0530

    updaed final upload of models

--- farmer/admin.py
+++ farmer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import farmer_details, customer_details, product_details, nutritionist_details, order_details, payment_details
 # Register your models here.
+admin.site.register(farmer_details)
+admin.site.register(customer_details)
+admin.site.register(product_details)
+admin.site.register(nutritionist_details)
+admin.site.register(order_details)
+admin.site.register(payment_details)
--- farmer/models.py
+++ farmer/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+# Farmer Profile
+
 
 class farmer_details(models.Model):
-    f_id = models.AutoField()
+    f_id = models.IntegerField()
     f_fname = models.CharField(max_length=25)
     f_lname = models.CharField(max_length=25)
     f_img = models.ImageField()
@@ -19,8 +21,9 @@ class farmer_details(models.Model):
         return self.f_fname + ' ' + self.f_lname
 
 
+# Customer Profile
 class customer_details(models.Model):
-    c_id = models.AutoField()
+    c_id = models.IntegerField()
     c_fname = models.CharField(max_length=25)
     c_lname = models.CharField(max_length=25)
     c_img = models.ImageField()
@@ -35,8 +38,9 @@ class customer_details(models.Model):
         return self.c_fname + ' ' + self.c_lname
 
 
+# Product Details
 class product_details(models.Model):
-    p_id = models.AutoField()
+    p_id = models.IntegerField()
     p_name = models.CharField(max_length=25)
     p_price = models.FloatField()
     p_type = models.CharField(max_length=25)
@@ -47,11 +51,25 @@ class product_details(models.Model):
     def __str__(self) -> str:
         return self.p_name
 
+# Nutritionist Details
 
-class farmer_order(models.Model):
-    o_fid = models.CharField()
-    o_id = models.AutoField()
-    o_pid = models.CharField()
+
+class nutritionist_details(models.Model):
+    n_id = models.IntegerField()
+    n_fname = models.CharField(max_length=25)
+    n_lname = models.CharField(max_length=25)
+    n_details = models.CharField(max_length=125)
+    n_contact = models.IntegerField()
+
+    def __str__(self):
+        return self.n_fname + ' ' + self.n_lname
+
+
+# Order Details
+class order_details(models.Model):
+    o_fid = models.IntegerField()
+    o_id = models.IntegerField()
+    o_pid = models.IntegerField()
     o_qty = models.PositiveIntegerField()
 
     def __str__(self) -> str:
@@ -59,10 +77,10 @@ class farmer_order(models.Model):
 
 
 class payment_details(models.Model):
-    pmt_id = models.AutoField()
-    pmt_oid = models.CharField()
+    pmt_id = models.IntegerField()
+    pmt_oid = models.IntegerField()
     pmt_amt = models.FloatField()
-    pmt_cid = models.CharField()
+    pmt_cid = models.IntegerField()
 
     def __str__(self) -> str:
         return self.pmt_id
