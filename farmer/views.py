5a02bdfe3241993a6d6faa0d014785cb577a4d3e
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2023-03-13 23:15:32 +0530

    Uploading Files

---
+++ farmer/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+# def about(request):
+#     return render(request, 'About.html')
+
+
+# def contact(request):
+#     return render(request, 'Contact.html')
+
+
+# def login(request):
+#     return render(request, 'Login.html')
+
+
+# def register(request):
+#     return render(request, 'Register.html')
